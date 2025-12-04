78630583b93d150e4758c59f2b85cde60b15fb17
Andylost8384 <108447206+Andylost8384@users.noreply.github.com>
2025-12-04 14:18:32 +0530

    Update videosData.js

--- videosData.js
+++ videosData.js
@@ -9,7 +9,8 @@ window.VIDEOS = [
     category: "Hentaied",
     duration: "18:44",
     createdAt: "2025-08-29",
-    thumb: "1.png",
+    // Thumbnail = Lia Lin image
+    thumb: "https://ik.imagekit.io/uno5sox6q/Lia%20Lin.jpg",
     previewUrl: "https://archive.org/download/feelac-1/Preview%20Secret%20Feelings%20Act%201.mp4",
     fullUrl: "https://archive.org/download/feelac-1/Feelac1.mp4"
   },
@@ -20,7 +21,7 @@ window.VIDEOS = [
     category: "Hentaied",
     duration: "13:47",
     createdAt: "2025-08-22",
-    thumb: "2.png",
+    thumb: "https://ik.imagekit.io/uno5sox6q/Ashby%20Winter.jpg",
     previewUrl: "https://archive.org/download/a-dangerous-encounter-freeze-1080p/Descending%20-%20Hentaied.mp4",
     fullUrl: "https://archive.org/download/4-k-60-fpsheav/Des1080p.mp4"
   },
@@ -31,7 +32,7 @@ window.VIDEOS = [
     category: "Hentaied",
     duration: "10:44",
     createdAt: "2025-08-29",
-    thumb: "3.png",
+    thumb: "https://ik.imagekit.io/uno5sox6q/Tiffany%20Tatum.jpg",
     previewUrl: "https://archive.org/download/feelac-1/Preview%20Descending%202%20-%20Hentaied.ia.mp4",
     fullUrl: "https://archive.org/download/4-k-60-fpsheav/Des21080p.mp4"
   },
@@ -42,7 +43,7 @@ window.VIDEOS = [
     category: "Hentaied",
     duration: "9:41",
     createdAt: "2025-10-31",
-    thumb: "4.png",
+    thumb: "https://ik.imagekit.io/uno5sox6q/Sata%20Jones.jpg",
     previewUrl: "https://archive.org/download/ending-3-1080p/Previewending3.mp4",
     fullUrl: "https://archive.org/download/ending-3-1080p/ending3%201080p.mp4"
   },
@@ -53,7 +54,7 @@ window.VIDEOS = [
     category: "Hentaied",
     duration: "20:02",
     createdAt: "2025-11-28",
-    thumb: "5.png",
+    thumb: "https://ik.imagekit.io/uno5sox6q/Lucy%20Mochi.jpg",
     previewUrl: "https://archive.org/download/ending-3-1080p/PreviewCocoon.mp4",
     fullUrl: "https://archive.org/download/ending-3-1080p/Coon1080p.mp4"
   },
@@ -64,7 +65,7 @@ window.VIDEOS = [
     category: "Lesbian",
     duration: "31:21",
     createdAt: "2022-12-15",
-    thumb: "2.png",
+    thumb: "https://ik.imagekit.io/uno5sox6q/Kelly%20Collins.jpg",
     previewUrl: "https://archive.org/download/yo-2-4k/Preview%20Yoga%201.mp4",
     fullUrl: "https://archive.org/download/yo-2-4k/1%201080p.mp4"
   },
@@ -75,7 +76,7 @@ window.VIDEOS = [
     category: "Lesbian",
     duration: "32:40",
     createdAt: "2023-02-09",
-    thumb: "3.png",
+    thumb: "https://ik.imagekit.io/uno5sox6q/Eve%20Sweet.jpg",
     previewUrl: "https://archive.org/download/yo-2-4k/Preview%20Yoga%202.mp4",
     fullUrl: "https://archive.org/download/yo-2-4k/2%201080p.mp4"
   }
